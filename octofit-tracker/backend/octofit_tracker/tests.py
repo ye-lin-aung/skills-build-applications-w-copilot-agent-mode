10748e76aa61fcb49b789858b7df28fa58537918
Y <hello.yelinaung@gmail.com>
2026-02-10 02:07:11 +0000

    Implement API views, serializers, and URL routing for core models

---
+++ octofit-tracker/backend/octofit_tracker/tests.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+from .models import User, Team, Activity, Leaderboard, Workout
+
+class UserModelTest(TestCase):
+    def test_create_user(self):
+        user = User.objects.create(email='test@hero.com', name='Test Hero', team='Marvel')
+        self.assertEqual(user.email, 'test@hero.com')
+
+class TeamModelTest(TestCase):
+    def test_create_team(self):
+        team = Team.objects.create(name='Test Team')
+        self.assertEqual(team.name, 'Test Team')
+
+class ActivityModelTest(TestCase):
+    def test_create_activity(self):
+        activity = Activity.objects.create(user='Test Hero', activity_type='Running', duration=10)
+        self.assertEqual(activity.activity_type, 'Running')
+
+class LeaderboardModelTest(TestCase):
+    def test_create_leaderboard(self):
+        leaderboard = Leaderboard.objects.create(team='Test Team', points=100)
+        self.assertEqual(leaderboard.points, 100)
+
+class WorkoutModelTest(TestCase):
+    def test_create_workout(self):
+        workout = Workout.objects.create(name='Test Workout', description='Test Desc')
+        self.assertEqual(workout.name, 'Test Workout')
--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -14,8 +14,31 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from .views import UserViewSet, TeamViewSet, ActivityViewSet, LeaderboardViewSet, WorkoutViewSet
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'teams', TeamViewSet)
+router.register(r'activities', ActivityViewSet)
+router.register(r'leaderboard', LeaderboardViewSet)
+router.register(r'workouts', WorkoutViewSet)
+
+@api_view(['GET'])
+def api_root(request):
+    return Response({
+        'users': request.build_absolute_uri('/users/'),
+        'teams': request.build_absolute_uri('/teams/'),
+        'activities': request.build_absolute_uri('/activities/'),
+        'leaderboard': request.build_absolute_uri('/leaderboard/'),
+        'workouts': request.build_absolute_uri('/workouts/'),
+    })
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', api_root, name='api-root'),
+    path('', include(router.urls)),
 ]
